3b360ac68b7a717a455ddf94fe6798800dad011e
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-06-17 10:13:25 +0900

    Feat: set sendNotification of Handler

--- LocalNotificationTest/LocalNotificationTest/Hadlers/NotificationHandler.swift
+++ LocalNotificationTest/LocalNotificationTest/Hadlers/NotificationHandler.swift
@@ -18,4 +18,22 @@ class NotificationHandler {
             }
         }
     }
+    
+    func sendNotification(date: Date, type: String, timeInterval: Double = 5, title: String, body: String) {
+        var trigger: UNNotificationTrigger?
+        if type == "date" {
+            let dateComponents = Calendar.current.dateComponents([.day, .month, .year, .hour, .minute], from: date)
+            trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
+        } else if type == "time" {
+            trigger = UNTimeIntervalNotificationTrigger(timeInterval: timeInterval, repeats: false)
+        }
+        
+        let contet = UNMutableNotificationContent()
+        contet.title = title
+        contet.body = body
+        contet.sound = UNNotificationSound.default
+        
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: contet, trigger: trigger)
+        UNUserNotificationCenter.current().add(request)
+    }
 }

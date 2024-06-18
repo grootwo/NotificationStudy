d8a07abc027aa002833a3f07f54e1bfed5674cda
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-06-18 09:38:52 +0900

    Feat: add repeat alarm type and its trigger button

--- LocalNotificationTest/LocalNotificationTest/Views/ContentView.swift
+++ LocalNotificationTest/LocalNotificationTest/Views/ContentView.swift
@@ -8,18 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var selectedDate = Date()
+    @State private var selectedTime = Date()
     let notify = NotificationHandler()
     
     var body: some View {
         VStack {
             Spacer()
+            DatePicker("Pick noti time: ", selection: $selectedTime, in: Date()..., displayedComponents: [.hourAndMinute])
+            Button("Send a notification at time") {
+                notify.sendNotification(date: selectedTime, type: "calendar", title: "Date Notification", body: "This is calendar notification")
+            }
             Button("Send a notification in 5 seconds") {
                 notify.sendNotification(date: Date(), type: "timeInterval", timeInterval: 5, title: "Time Notification", body: "This is timeinterval notification")
             }
-            DatePicker("Pick noti time: ", selection: $selectedDate, in: Date()...)
-            Button("Send a notification at time") {
-                notify.sendNotification(date: selectedDate, type: "calendar", title: "Date Notification", body: "This is calendar notification")
+            Button("Send a notification repeatedly") {
+                notify.sendNotification(date: selectedTime, type: "repeat", title: "Repeat Notification", body: "This is repeat notification")
             }
             Spacer()
             Text("Not working?")
